ca41a4d354ad312eb534df8079bd34148207bde5
Ross Ritter <rossritter@Rosss-MBP.attlocal.net>
2023-11-09 14:36:16 -0500

    cant get the background color to cchange might not be linked up poperly

--- React-Router/src/App.jsx
+++ React-Router/src/App.jsx
@@ -1,8 +1,11 @@
 import { useState } from 'react'
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
+import Blue from './components/Blue';
+import Red from './components/Red';
+import Home from './components/Home';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,11 +14,16 @@ function App() {
     <>
       <div id="container">
       {/* <h1>Hello React Router!</h1> */}
-      <div id="navbar">{/* navigation here */}</div>
+      <div id="navbar">
+        <Link to= "/Blue" > Blue </Link>
+        <Link to= "/Red" > Red </Link>
+        <Link to= "/"> Home </Link>
+      </div>
       <div id="main-section">
         <Routes>
-        <Route path="/blue" element={<h1>Blue</h1>} />
-        <Route path="/red" element={<h1>Red</h1>} />
+        <Route path="/Blue" element={Blue} />
+        <Route path="/Red" element={Red} />
+        <Route path="/Home" element={Home}/>
 
         </Routes>
       </div>
---
+++ React-Router/src/components/Home.jsx
@@ -0,0 +1,10 @@
+
+function Home(){
+<div className="home"></div>
+  return 
+
+    <h1>home</h1>
+  
+}
+
+export default Home

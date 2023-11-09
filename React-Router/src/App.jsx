1440d81c2806491c8645034db5b253cf9175c7b5
Ross Ritter <rossritter@Rosss-MBP.attlocal.net>
2023-11-09 14:02:11 -0500

    finished with Router Conf

--- React-Router/src/App.jsx
+++ React-Router/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Routes, Route } from "react-router-dom";
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -9,9 +10,15 @@ function App() {
   return (
     <>
       <div id="container">
-      <h1>Hello React Router!</h1>
+      {/* <h1>Hello React Router!</h1> */}
       <div id="navbar">{/* navigation here */}</div>
-      <div id="main-section">{/* routes here */}</div>
+      <div id="main-section">
+        <Routes>
+        <Route path="/blue" element={<h1>Blue</h1>} />
+        <Route path="/red" element={<h1>Red</h1>} />
+
+        </Routes>
+      </div>
     </div>
     </>
   )
